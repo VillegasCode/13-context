9e48683de60b21932afb3ec022b35099991e95fe
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-21 21:07:29 -0500

    const {usuario, setUsuario} = useContext(PruebaContext) and use like usuario.nombre and usuario.web

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,14 +3,18 @@ import { PruebaContext } from '../context/PruebaContext'
 
 export const Inicio = () => {
   
-  const compartida = useContext(PruebaContext);
+  // const compartida = useContext(PruebaContext);
 
   // Ya se puede accesar al valor de PruebaContext.js a través de la const compartida
   
+  const {usuario, setUsuario} = useContext(PruebaContext);
+
   return (
     <div>
       <h1>Inicio</h1>
-      <p>Valor compartido: <strong>{compartida.titulo}</strong></p>
+      <p>Nombre: {usuario.nombre}</p>
+      <p>Web: {usuario.web}</p>
+      {/* <p>Valor compartido: <strong>{compartida.titulo}</strong></p> */}
     </div>
   )
 }
