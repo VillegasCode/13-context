c5b39e7c4f678faf9c330b122ec587cf4adb01b5
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-21 19:40:44 -0500

    Creating object named curso and enclose AppRouter into PruebaContext.provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { PruebaContext } from './context/PruebaContext';
 import { AppRouter } from './routing/AppRouter';
 
 function App() {
+
+  // Pasando este objeto como valor de PruebaContext.provider
+  const curso = {
+    id: 1,
+    titulo: "Master en TypeScript",
+    contenido: "Muchas horas de contenido..."
+  };
+
   return (
     <div className="App">
-      
-      <AppRouter />
-      
+      <PruebaContext.Provider value={curso}>
+        <AppRouter />
+      </PruebaContext.Provider>
     </div>
   );
 }
