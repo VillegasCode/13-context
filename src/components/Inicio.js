ca9ade4a6bbf31254957870772474033c30fdd44
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-21 19:44:55 -0500

    Importing PruebaContext and creating a const named compartida and using a hook named useContext and putting into <p> like compartida.titulo

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PruebaContext } from '../context/PruebaContext'
 
 export const Inicio = () => {
+  
+  const compartida = useContext(PruebaContext);
+
+  // Ya se puede accesar al valor de PruebaContext.js a través de la const compartida
+  
   return (
-    <div>Inicio</div>
+    <div>
+      <h1>Inicio</h1>
+      <p>Valor compartido: <strong>{compartida.titulo}</strong></p>
+    </div>
   )
 }
