7d1853c814c59f10e5f52190c928065676739bbe
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-24 21:05:57 -0500

    2 useEffect created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { PruebaContext } from './context/PruebaContext';
 import { AppRouter } from './routing/AppRouter';
 
 function App() {
 
-  const [usuario, setUsuario] = useState({
-    nick: "@carlosAlex",
-    nombre: "Carlos",
-    web: "empresarioencasa.org"
-  });
+  const [usuario, setUsuario] = useState({});
 
+  useEffect(() => {
+    console.log("USE EFFECT UNA VEZ")
+    //La primera vez que se carga el componente
+    let usuario_local = JSON.parse(localStorage.getItem("usuario"));
+
+    setUsuario(usuario_local);
+  }, []);
+
+  useEffect(() => {
+    console.log("CAMBIO EN ESTADO USUARIO")
+    //Cada vez que se actualiza el estado usuario se guarda en el LS
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+  },[usuario]);
+  
   // Pasando este objeto como valor de PruebaContext.provider
   const curso = {
     id: 1,
